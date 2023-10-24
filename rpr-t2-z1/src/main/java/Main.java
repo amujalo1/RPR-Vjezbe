8ef737d2c1e73ee494c0ac03c3850fb242946bda
amujalo1 <amujalo1@etf.unsa.ba>
2023-10-24 14:03:32 +0200

    dodao z3

---
+++ rpr-t2-z1/src/main/java/Main.java
@@ -0,0 +1,28 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import ba.unsa.etf.rpr.Kalkulator;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Molim unesite točno jedan broj kao argument.");
+        } else {
+            try {
+                Kalkulator cal = new Kalkulator();
+                double sinus = cal.Sin(Double.parseDouble(args[0]));
+                long fact = cal.Fact((int)Double.parseDouble(args[0]));
+                System.out.println("sin(" + Double.parseDouble(args[0]) + ") = " + sinus);
+                System.out.println((int)Double.parseDouble(args[0]) + "! = " + fact);
+            } catch (NumberFormatException var6) {
+                System.out.println("Uneseni argument nije valjani broj.");
+            }
+
+        }
+    }
+}
