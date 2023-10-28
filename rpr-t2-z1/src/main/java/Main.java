0e579f2e7c80805c7c11d1d09ba7b0cf725137c0
amujalo1 <amujalo1@etf.unsa.ba>
2023-10-28 21:25:34 +0200

    omogucio unos 2 varijable iz .jar dat

--- rpr-t2-z1/src/main/java/Main.java
+++ rpr-t2-z1/src/main/java/Main.java
@@ -10,15 +10,15 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Molim unesite točno jedan broj kao argument.");
+        if (args.length != 2) {
+            System.out.println("Molim unesite tocno jedan broj kao argument.");
         } else {
             try {
                 Kalkulator cal = new Kalkulator();
                 double sinus = cal.Sin(Double.parseDouble(args[0]));
-                long fact = cal.Fact((int)Double.parseDouble(args[0]));
+                long fact = cal.Fact((int)Double.parseDouble(args[1]));
                 System.out.println("sin(" + Double.parseDouble(args[0]) + ") = " + sinus);
-                System.out.println((int)Double.parseDouble(args[0]) + "! = " + fact);
+                System.out.println((int)Double.parseDouble(args[1]) + "! = " + fact);
             } catch (NumberFormatException var6) {
                 System.out.println("Uneseni argument nije valjani broj.");
             }
